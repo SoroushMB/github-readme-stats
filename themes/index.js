a400dde86922b905626e00fb055290cb71f2a50a
Soroush MasoomBabaei <68003197+SoroushMB@users.noreply.github.com>
2022-07-31 14:56:40 +0430

    Update index.js

--- themes/index.js
+++ themes/index.js
@@ -361,6 +361,13 @@ const themes = {
     text_color: "e0def4",
     bg_color: "191724",
   }
+  Starfire: {
+    title_color: "a277ff",
+    icon_color: "ffca85",
+    text_color: "61ffca",
+    bg_color: "15141b",
+    border_color: "0c1a25",
+  },
 };
 
 module.exports = themes;
